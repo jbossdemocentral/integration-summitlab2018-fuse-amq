0161b5823295c623be56bfdf48538c5bba6deca5
weimeilin79 <weimeilin@gmail.com>
2018-05-02 17:20:17 -0400

    add script

--- support/install/deployIgnite.sh
+++ support/install/deployIgnite.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-MASTER=https://master.3f1c.openshift.opentlc.com/
+MASTER=https://master.hackathon01.openshiftworkshop.com/
 OCP_PWD=r3dh4t1!
-ROUTE=apps.3f1c.openshift.opentlc.com
+ROUTE=apps.hackathon01.openshiftworkshop.com
 
 
-for userno in {1..3}
+for userno in {1..35}
 do
 
  echo Setup user$userno 
 
  oc login $MASTER -u user$userno -p $OCP_PWD 
  oc new-project user$userno
- oc create -f https://raw.githubusercontent.com/syndesisio/syndesis/master/install/syndesis.yml
- oc create -f https://raw.githubusercontent.com/syndesisio/syndesis/master/install/support/serviceaccount-as-oauthclient-restricted.yml
+ oc create -f syndesis.yml
+ oc create -f support/serviceaccount-as-oauthclient-restricted.yml
 
 
  oc new-app syndesis \
@@ -23,7 +23,7 @@ do
     -p OPENSHIFT_OAUTH_CLIENT_SECRET=$(oc sa get-token syndesis-oauth-client)
 
 
- oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/amq/amq63-basic.json
+ oc create -f support/syndesis-amq.yml
  
  oc new-app amq63-basic \
     -p MQ_USERNAME=amq \
